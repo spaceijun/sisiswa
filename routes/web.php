8e112e214019e465d9c06a330a57f8db6ae8a52c
ijunnn <faizunaziz53@gmail.com>
2025-06-13 16:07:03 +0700

    Update : Redirect Login Page

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\Superadmin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
